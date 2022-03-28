9696af269416b466873750bc7ad10e9cf016e3a4
nabenitez <nabenitez@uc.cl>
2022-03-28 20:06:33 -0300

    fix(components): export name

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,6 +3,6 @@ export { default as HeroCard } from './hero-card';
 export { default as LoadingScreen } from './loading-screen';
 export { default as NavBar } from './navbar';
 export { default as SearchField } from './searchfield';
-export { default as DetailCard } from './detail-card';
+export { default as DetailedCard } from './detailed-card';
 export { default as ErrorScreen } from './error-screen';
 export { default as FiltersAutocomplete } from './filters-autocomplete';
