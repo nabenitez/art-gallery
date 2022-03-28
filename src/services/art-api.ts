bec58706525eb86f691918f49c0ab0a9bf589aa1
nabenitez <nabenitez@uc.cl>
2022-03-27 22:12:53 -0300

    feat(services): add requests functions

---
+++ src/services/art-api.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+const API_URL: string = process.env.NEXT_PUBLIC_ART_API_URL;
+
+const axiosClient = axios.create({
+  baseURL: API_URL,
+  timeout: 1000,
+});
+
+export const fetchArtWorks = async (page: number = 1) => {
+  const { data } = await axiosClient.get(`?page=${page}`);
+  return data;
+};
+
+export const searchArtWorks = async (query: string, page: number = 1) => {
+  const { data } = await axiosClient.get(
+    `/search?q=${query}?page=${page}&limit=12&fields=id,title,image_id,exhibition_history,category_titles`,
+  );
+  return data;
+};
+
+export const filterArtWorks = async (query: string, page: number = 1) => {
+  const { data } = await axiosClient.get(`/search?q=${query}?page=${page}`);
+  return data;
+};
+
+export const getArtWorkById = async (id: string) => {
+  const { data } = await axiosClient.get(`/${id}`);
+  return data;
+};
