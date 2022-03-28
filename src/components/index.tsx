a82c54131ebc154251dbe8b07a390020e8e786fe
nabenitez <nabenitez@uc.cl>
2022-03-27 22:10:24 -0300

    feat(components): add index exports

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+export { default as AppLayout } from './app-layout';
+export { default as HeroCard } from './hero-card';
+export { default as LoadingScreen } from './loading-screen';
+export { default as NavBar } from './navbar';
+export { default as SearchField } from './searchfield';
+export { default as DetailCard } from './detail-card';
+export { default as ErrorScreen } from './error-screen';
