1aeeff0ec5756abc76313bcc07c6d4ce4f12e101
nabenitez <nabenitez@uc.cl>
2022-03-28 14:41:28 -0300

    feat(utils): add getFilterQuery to request search art api

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -5,3 +5,33 @@
  */
 export const getImageUrl = (identifier: string): string =>
   `https://www.artic.edu/iiif/2/${identifier}/full/843,/0/default.jpg`;
+
+/**
+ *
+ * @param filters strings array with keys to filter data
+ * @returns
+ */
+export const getFilterQuery = (filters: string[]) => {
+  const getConditions = () =>
+    filters.map((filter) => ({
+      match: {
+        artwork_type_title: filter,
+      },
+    }));
+  const getQueryBase = (conditions: object[]) => ({
+    query: {
+      bool: {
+        must: [
+          {
+            bool: {
+              should: conditions,
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  const composedQuery = getQueryBase(getConditions());
+  return encodeURIComponent(JSON.stringify(composedQuery));
+};
