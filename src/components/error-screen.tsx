07cd3f85f495d8c92d802fb23f7cbae3d7f655b0
nabenitez <nabenitez@uc.cl>
2022-03-28 14:42:45 -0300

    refactor(components): error screen image and text improvement

--- src/components/error-screen.tsx
+++ src/components/error-screen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import useImageError from '../hooks/use-image-error';
 
 const ErrorScreen = () => {
@@ -8,7 +8,7 @@ const ErrorScreen = () => {
   return !imgError ? (
     <Box
       sx={{
-        width: '60%',
+        width: '40%',
         margin: '15vh auto',
       }}
     >
@@ -17,14 +17,18 @@ const ErrorScreen = () => {
           borderRadius: 8,
         }}
         src={
-          'https://i.pinimg.com/originals/b8/b8/f7/b8b8f787c454cf1ded2d9d870707ed96.png'
+          'https://www.laborum.cl/candidate/static/media/algo-no-salio-bien.d1d4c9ba.png'
         }
         alt="Error has ocurred"
         onError={handleImgError}
-        width={800}
-        height={600}
+        width={550}
+        height={350}
         layout="responsive"
       />
+      <Typography component="div" variant="h6" color="secondary">
+        Ooops. Something wen&apos;t wrong, please refresh the page or try again
+        later.
+      </Typography>
     </Box>
   ) : null;
 };
